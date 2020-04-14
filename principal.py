5892747cdf31c40e44e5736b5cda2d9d83840f54
lsarteaga <lsarteaga_dev@protonmail.ch>
2020-04-13 19:02:46 -0500

    creacion de la ventana para agregar inquilino(sin funcionalidad), agregacion de la base de datos

--- principal.py
+++ principal.py
@@ -24,7 +24,22 @@ class Aplicacion:
         self.ventana_principal.mainloop()
         
     def agregar_inquilino(self):
-        pass
+        self.pagina_agregar = ttk.Frame(self.notebook)
+        self.notebook.add(self.pagina_agregar, text = 'Agregar Inquilino')
+        self.label_frame = ttk.Labelframe(self.pagina_agregar, text = 'Datos inquilino')
+        self.label_frame.grid(column = 0, row = 0, padx = 5, pady = 10)
+        #labels y entrys para ingreso de datos
+        ttk.Label(self.label_frame, text = 'Nombre: ').grid(column = 0, row = 0, padx = 4, pady = 4)
+        self.nombre_inquilino =tk.StringVar()
+        ttk.Entry(self.label_frame, textvariable = self.nombre_inquilino).grid(column = 1, row = 0, padx = 4, pady = 4)
+        ttk.Label(self.label_frame, text = 'Cedula: ').grid(column = 0, row = 1, padx = 4, pady = 4)
+        self.cedula_inquilino =tk.StringVar()
+        ttk.Entry(self.label_frame, textvariable = self.cedula_inquilino).grid(column = 1, row = 1, padx = 4, pady = 4)
+        ttk.Label(self.label_frame, text = 'Celular: ').grid(column = 0, row = 2, padx = 4, pady = 4)
+        self.celular_inquilino =tk.StringVar()
+        ttk.Entry(self.label_frame, textvariable = self.celular_inquilino).grid(column = 1, row = 2, padx = 4, pady = 4)
+        #boton para agregar
+        ttk.Button(self.label_frame, text = 'Agregar').grid(column = 0, row = 3, columnspan = 2, sticky = 'we', padx = 4, pady = 10)
     
     def listado_inquilinos(self):
         pass
